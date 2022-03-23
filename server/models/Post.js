82b57579f9af5c5425067c778adbea95624cbb1c
Rodney P <rodney.plancher@gmail.com>
2022-03-23 16:20:55 -0400

    updating models, user need virtuals for theirdxddfvdfbdfvdfdfvdfvfvfvfvfvfvfvfvfvfvfv

--- server/models/Post.js
+++ server/models/Post.js
@@ -2,6 +2,9 @@ const { Schema, model } = require("mongoose");
 const PostSchema = new Schema({
   stockName: {
     type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 6,
   },
   createdAt: {
     type: Date,
@@ -13,13 +16,18 @@ const PostSchema = new Schema({
   },
   amountBought: {
     type: Number,
+    required: true,
+    minlength: 2,
+    maxlength: 6,
   },
   pricedAt: {
     type: Number,
+    required: true,
+    minlength: 2,
+    maxlength: 6,
   },
 });
 // create the Post model using the PostSchema
 const Post = model("Post", PostSchema);
-
 // export the Post model
 module.exports = Post;
--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,49 @@
 const { Schema, model } = require("mongoose");
 const UserSchema = new Schema({
-  email:
-  username:String,
-  password:String,
-  
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+
+  // need virtuals for post. going to look them up later
+  //
+  //
+  //
+  //
+  //
+  //
+  //
 });
+
+// set up middleware to create password
+//
+//
+//
+//
+//
+//
+// compare the incoming password with the hashed password
+//
+//
+//
+//
+//
+//
+
 // create the User model using the UserSchema
 const User = model("User", UserSchema);
-
 // export the User model
 module.exports = User;
