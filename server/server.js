9d289f8e4cba82bf041040ea6cc5ab4ca11fe440
Rodney P <rodney.plancher@gmail.com>
2022-03-23 12:58:10 -0400

    file structure update

--- server/server.js
+++ server/server.js
@@ -1,32 +1,14 @@
-const { ApolloServer } = require("apollo-server-express");
 const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
 const mongoose = require("mongoose");
-const gql = require("graphql-tag");
+const path = require("path");
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 const Post = require("./models/Post");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-// const typeDefs = gql`
-// type Post{
-//   id:ID
-// }
-// type Query{
-//   getPosts
-// }
-// `;
-
-// const resolvers = {
-//   Query: {
-//     getPosts,
-//   },
-// };
-
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-// });
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://127.0.0.1/asset-manager",
@@ -36,6 +18,6 @@ mongoose.connect(
 // Use this to log mongo queries being executed!
 mongoose.set("debug", true);
 
-app.use(require("./routes"));
+// app.use(require("./routes"));
 
 app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
